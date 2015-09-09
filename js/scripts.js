d762b42349cc0db2b5ee8834acdb32e61028ddf1
Andrew Brennwald & Ryan Heupler <student@epicodus.com>
2015-09-09 13:45:43 -0700

    Implement front-end.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
 var palindrome = function(input) {
-  return input = input.split("").reverse().join("") === input;
+  return input.toLowerCase().split("").reverse().join("") === input.toLowerCase();
 }
+
+$(document).ready(function() {
+  $("form#input").submit(function(event) {
+    var input = $("input#word").val();
+    var result = palindrome(input);
+
+    $(".palindrome").text(result);
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
